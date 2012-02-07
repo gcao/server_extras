b446ae5bc2efd77e3c4244fd9679734a5baad2dc
Guoliang Cao <gcao99@gmail.com>
2012-02-07 17:50:56 -0500

    Update backup and restore scripts

--- backup.sh
+++ backup.sh
@@ -4,10 +4,14 @@ rm -rf ~/backup
 mkdir ~/backup
 cd ~/backup
 
+stop mysql
+
 tar czf mysql-bbs.tgz     -C /var/lib/mysql bbs
 tar czf mysql-gocool.tgz  -C /var/lib/mysql gocool_production
 tar czf mysql-ucenter.tgz -C /var/lib/mysql ucenter
 
 tar czf bbs-images.tgz    -C /data/apps/bbs/current attachments images/avatars/bbsxp
 tar czf gocool-sgfs.tgz   -C /data/apps/gocool/shared system
+
+start mysql
 
--- restore.sh
+++ restore.sh
@@ -2,11 +2,16 @@
 
 /etc/init.d/apache2 stop
 
+rm -rf /var/lib/mysql/bbs /var/lib/mysql/ucenter /var/lib/mysql/gocool_production
 tar xzf ~/backup/mysql-bbs.tgz     -C /var/lib/mysql
 tar xzf ~/backup/mysql-ucenter.tgz -C /var/lib/mysql
 tar xzf ~/backup/mysql-gocool.tgz  -C /var/lib/mysql
 
 restart mysql
+
+rm -rf /data/apps/bbs/current/attachments /data/apps/bbs/current/images/avatars/bbsxp
+rm -rf /data/apps/gocool/current/shared/system
+
 /etc/init.d/apache2 start
 
 tar xzf ~/backup/bbs-images.tgz    -C /data/apps/bbs/current
